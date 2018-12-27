3e53f1c2b99b99313bd963979644ff47dd1e97cf
Chris Schrijver <chris@raow.nl>
2018-12-27 15:30:52 +0100

    update helpers.php

--- src/Foundation/helpers.php
+++ src/Foundation/helpers.php
@@ -1,10 +1,31 @@
 <?php
-if (!function_exists('offbeatWP')) {
+if (!function_exists('offbeat')) {
     function offbeat($service = null) {
         if (!is_null($service)) {
             return container($service);
         }
 
         return \OffbeatWP\Foundation\App::singleton();
+    }
+}
+
+if (!function_exists('config')) {
+    function config($config = null, $default = null) {
+        return offbeat()->config($config, $default);
+    }
+}
+
+if (!function_exists('container')) {
+    function container($definition = null) {
+        if (!is_null($definition))
+            return offbeat()->container->get($definition);
+
+        return offbeat()->container;
+    }
+}
+
+if (!function_exists('assetUrl')) {
+    function assetUrl($file) {
+        return offbeat('assets')->getUrl($file);
     }
 }
