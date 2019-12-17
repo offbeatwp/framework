04de743308690254651c45d5b3bf09220bcc1002
Chris Schrijver <chris@raow.nl>
2019-12-17 11:27:13 +0100

    Maybe redirect post if url doesnt match

--- src/Support/Wordpress/Post.php
+++ src/Support/Wordpress/Post.php
@@ -1,16 +1,20 @@
 <?php
 namespace OffbeatWP\Support\Wordpress;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class Post
 {
-    public function convertWpPostToModel(\WP_Post $post) {
+    public function convertWpPostToModel(\WP_Post $post)
+    {
         $model = offbeat('post-type')->getModelByPostType($post->post_type);
         $model = offbeat('hooks')->applyFilters('post_model', $model, $post);
 
         return new $model($post);
     }
 
-    public function get($id = null) {
+    public function get($id = null)
+    {
         if (is_null($id)) $id = get_the_ID();
 
         $post = get_post($id);
@@ -20,5 +24,22 @@ class Post
         }
 
         return null;
+    }
+
+    public function maybeRedirect($post)
+    {
+        $request = Request::create($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_REQUEST, $_COOKIE, [], $_SERVER);
+        $requestUri = strtok($request->getUri(), '?');
+        $postUri    = $post->getPermalink();
+
+        if ($requestUri !== $postUri) {
+            $url = $post->getPermalink();
+
+            if (!empty($_GET))
+                $url .= '?' . http_build_query($_GET);
+
+            offbeat('http')->redirect($url);
+            exit;
+        }
     }
 }
