203ad4596a11efe9e905134d5a8f9e92851e07a5
Chris Schrijver <chris@raow.nl>
2020-03-06 13:53:06 +0100

    return empty collection on ancestors in stead of false when no results

--- src/Content/Post/PostModel.php
+++ src/Content/Post/PostModel.php
@@ -295,16 +295,15 @@ class PostModel implements PostModelInterface
 
     public function getAncestors()
     {
+        $ancestors = collect();
+
         if ($this->hasParent()) {
-            $ancestors = collect();
             foreach ($this->getAncestorIds() as $ancestorId) {
                 $ancestors->push(offbeat('post')->get($ancestorId));
             }
-
-            return $ancestors;
         }
 
-        return false;
+        return $ancestors;
     }
 
     public function getPreviousPost($inSameTerm = false, $excludedTerms = '', $taxonomy = 'category')
