28ec43a68a99763de1a0030bb0ffedcc5ba623f9
Chris Schrijver <chris@raow.nl>
2020-08-12 09:52:11 +0200

    fix issues on relationsships with sticky posts

--- src/Content/Post/Relations/BelongsTo.php
+++ src/Content/Post/Relations/BelongsTo.php
@@ -8,6 +8,7 @@ class BelongsTo extends BelongsToOneOrMany
     public function query()
     {
         return (new WpQueryBuilder())
+            ->where(['ignore_sticky_posts' => 1])
             ->wherePostType('any')
             ->hasRelationshipWith($this->model, $this->key, 'reverse');
     }
