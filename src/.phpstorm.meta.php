0d67e54e7ed3111a9dc85a73a7b794f278c7ccff
Maikel Haarmans <maikel@raow.nl>
2021-04-02 15:09:41 +0200

    Container now behaves the same

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -16,4 +16,20 @@ namespace PHPSTORM_META {
         'routes' => \OffbeatWP\Routes\RoutesManager::class,
         'assets' => \OffbeatWP\Assets\AssetsManager::class
     ]));
+
+    override(\container(), map([
+        'admin-page' => \OffbeatWP\Support\Wordpress\AdminPage::class,
+        'ajax' => \OffbeatWP\Support\Wordpress\Ajax::class,
+        'rest-api' => \OffbeatWP\Support\Wordpress\RestApi::class,
+        'console' => \OffbeatWP\Support\Wordpress\Console::class,
+        'hooks' => \OffbeatWP\Support\Wordpress\Hooks::class,
+        'post-type' => \OffbeatWP\Support\Wordpress\PostType::class,
+        'post' => \OffbeatWP\Support\Wordpress\Post::class,
+        'page' => \OffbeatWP\Support\Wordpress\Page::class,
+        'taxonomy' => \OffbeatWP\Support\Wordpress\Taxonomy::class,
+        'design' => \OffbeatWP\Support\Wordpress\Design::class,
+        'components' => \OffbeatWP\Components\ComponentRepository::class,
+        'routes' => \OffbeatWP\Routes\RoutesManager::class,
+        'assets' => \OffbeatWP\Assets\AssetsManager::class
+    ]));
 }
