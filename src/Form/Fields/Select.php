9103883379914f19af427cad1a4143391bd7142f
Maikel Haarmans <maikel@raow.nl>
2021-06-25 11:27:03 +0200

    Fix a regression in Select.php

--- src/Form/Fields/Select.php
+++ src/Form/Fields/Select.php
@@ -4,4 +4,15 @@ namespace OffbeatWP\Form\Fields;
 class Select extends FieldWithOptions
 {
     const FIELD_TYPE = 'select';
+
+    /**
+     * @param string[] $options
+     * @return $this
+     */
+    public function addOptions($options = []): FieldWithOptions
+    {
+        $this->options = array_replace($this->options, $options);
+
+        return $this;
+    }
 }
