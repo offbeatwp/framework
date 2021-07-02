768e7159dc0b9206a06a9fde675dde048c7aea31
Maikel Haarmans <maikel@raow.nl>
2021-07-02 16:37:07 +0200

    Collections will now type-hint their respective models when extracting single values with methods such as 'first'

--- src/Content/Post/PostsCollection.php
+++ src/Content/Post/PostsCollection.php
@@ -2,6 +2,7 @@
 namespace OffbeatWP\Content\Post;
 
 use Illuminate\Support\Collection;
+use WP_Post;
 
 class PostsCollection extends Collection
 {
@@ -22,7 +23,7 @@ class PostsCollection extends Collection
             $postItems = null;
         } elseif (is_array($items)) {
             foreach ($items as $itemKey => $item) {
-                if ($item instanceof \WP_Post) {
+                if ($item instanceof WP_Post) {
                     $items[$itemKey] = offbeat('post')->convertWpPostToModel($item);
                 }
             }
@@ -31,12 +32,48 @@ class PostsCollection extends Collection
         parent::__construct($items);
     }
 
-    public function getIterator()
+    public function getIterator(): WpPostsIterator
     {
         return new WpPostsIterator($this->items);
     }
 
     public function getQuery() {
         return $this->query;
+    }
+
+    /** @return PostModel|null */
+    public function first(callable $callback = null, $default = null)
+    {
+        return parent::first($callback, $default);
+    }
+
+    /** @return PostModel|null */
+    public function last(callable $callback = null, $default = null)
+    {
+        return parent::last($callback, $default);
+    }
+
+    /** @return PostModel|null */
+    public function pop()
+    {
+        return parent::pop();
+    }
+
+    /** @return PostModel|null */
+    public function pull($key, $default = null)
+    {
+        return parent::pull($key, $default);
+    }
+
+    /** @return PostModel|null */
+    public function reduce(callable $callback, $initial = null)
+    {
+        return parent::reduce($callback, $initial);
+    }
+
+    /** @return PostModel|null */
+    public function shift()
+    {
+        return parent::shift();
     }
 }
--- src/Content/Taxonomy/TermsCollection.php
+++ src/Content/Taxonomy/TermsCollection.php
@@ -5,5 +5,39 @@ use Illuminate\Support\Collection;
 
 class TermsCollection extends Collection
 {
+    /** @return TermModel|null */
+    public function first(callable $callback = null, $default = null)
+    {
+        return parent::first($callback, $default);
+    }
 
+    /** @return TermModel|null */
+    public function last(callable $callback = null, $default = null)
+    {
+        return parent::last($callback, $default);
+    }
+
+    /** @return TermModel|null */
+    public function pop()
+    {
+        return parent::pop();
+    }
+
+    /** @return TermModel|null */
+    public function pull($key, $default = null)
+    {
+        return parent::pull($key, $default);
+    }
+
+    /** @return TermModel|null */
+    public function reduce(callable $callback, $initial = null)
+    {
+        return parent::reduce($callback, $initial);
+    }
+
+    /** @return TermModel|null */
+    public function shift()
+    {
+        return parent::shift();
+    }
 }
