343aac28d354bf39de3f3943ead33645d39f30a5
Maikel Haarmans <maikel@raow.nl>
2021-10-26 09:46:26 +0200

    Change exception thrown by findPathRoute when a path is not found and make it return null rather than false

--- src/Routes/RoutesManager.php
+++ src/Routes/RoutesManager.php
@@ -3,7 +3,6 @@
 namespace OffbeatWP\Routes;
 
 use Closure;
-use Exception;
 use OffbeatWP\Exceptions\InvalidRouteException;
 use OffbeatWP\Routes\Routes\CallbackRoute;
 use OffbeatWP\Routes\Routes\PathRoute;
@@ -11,12 +10,13 @@ use OffbeatWP\Routes\Routes\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route as SymfonyRoute;
 
 class RoutesManager
 {
     protected $actions;
     protected $routeCollection;
-    protected $routesContext;
+    protected $routesContext; //TODO: Is this parameter used?
     protected $routeIterator = 0;
     protected $lastMatchRoute;
 
@@ -32,67 +32,64 @@ class RoutesManager
     }
 
     /** Callbacks are executed in LiFo order. */
-    public function callback($checkCallback, $actionCallback, $parameters = [], array $options = [])
+    public function callback($checkCallback, $actionCallback, $parameters = [], array $options = []): void
     {
         $this->addRoute($checkCallback, $actionCallback, $parameters, [], $options);
     }
 
-    public function get($route, $actionCallback, $parameters = [], array $requirements = [], array $options = [])
+    public function get($route, $actionCallback, $parameters = [], array $requirements = [], array $options = []): void
     {
         $this->addRoute($route, $actionCallback, $parameters, $requirements, $options, '', [], ['GET']);
     }
 
-    public function post(string $route, $actionCallback, array $parameters = [], array $requirements = [], array $options = [])
+    public function post(string $route, $actionCallback, array $parameters = [], array $requirements = [], array $options = []): void
     {
         $this->addRoute($route, $actionCallback, $parameters, $requirements, $options, '', [], ['POST']);
     }
 
-    public function put(string $route, $actionCallback, array $parameters = [], array $requirements = [], array $options = [])
+    public function put(string $route, $actionCallback, array $parameters = [], array $requirements = [], array $options = []): void
     {
         $this->addRoute($route, $actionCallback, $parameters, $requirements, $options, '', [], ['PUT']);
     }
 
-    public function patch($route, $actionCallback, $parameters = [], array $requirements = [], array $options = [])
+    public function patch($route, $actionCallback, $parameters = [], array $requirements = [], array $options = []): void
     {
         $this->addRoute($route, $actionCallback, $parameters, $requirements, $options, '', [], ['PATCH']);
     }
 
-    public function delete($route, $actionCallback, $parameters = [], array $requirements = [], array $options = [])
+    public function delete($route, $actionCallback, $parameters = [], array $requirements = [], array $options = []): void
     {
         $this->addRoute($route, $actionCallback, $parameters, $requirements, $options, '', [], ['DELETE']);
     }
 
     /** @param string|Closure $target */
-    public function addRoute($target, $actionCallback, $defaults, array $requirements = [], array $options = [], ?string $host = '', $schemes = [], $methods = [], ?string $condition = '')
+    public function addRoute($target, $actionCallback, $defaults, array $requirements = [], array $options = [], ?string $host = '', $schemes = [], $methods = [], ?string $condition = ''): void
     {
         $name = $this->getNextRouteName();
 
-        $routeClass = PathRoute::class;
-        if (!is_string($target)) {
-            $routeClass = CallbackRoute::class;
-        }
+        $routeClass = is_string($target) ? PathRoute::class : CallbackRoute::class;
 
         if ($defaults instanceof Closure) {
             $defaults = ['_parameterCallback' => $defaults];
         }
 
         $route = new $routeClass(
-            $name, //name
-            $target, // target
+            $name,
+            $target,
             $actionCallback,
-            $defaults, // default values
-            $requirements, // requirements
-            $options, // options
-            $host, // host
-            $schemes, // schemes
-            $methods, // methods
-            $condition // condition
+            $defaults,
+            $requirements,
+            $options,
+            $host,
+            $schemes,
+            $methods,
+            $condition
         );
 
         $this->getRouteCollection()->add($route->getName(), $route);
     }
 
-    public function findRoute()
+    public function findRoute(): ?SymfonyRoute
     {
         $route = $this->findPathRoute();
 
@@ -105,9 +102,8 @@ class RoutesManager
         return $route;
     }
 
-    public function findPathRoute()
+    public function findPathRoute(): ?SymfonyRoute
     {
-        // $request = Request::createFromGlobals(); // Disabled, gave issues with uploads
         $request = Request::create($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_REQUEST, $_COOKIE, [], $_SERVER);
 
         $context = new RequestContext();
@@ -121,51 +117,42 @@ class RoutesManager
                 throw new InvalidRouteException('Route does not match (override)');
             }
 
-            $route = $this->getRouteCollection()->get($parameters['_route']);
+            $route = $this->getRouteCollection()->getOrFail($parameters['_route']);
+
             $route->addDefaults($parameters);
 
             return $route;
-        } catch (Exception $e) {
-            return false;
+        } catch (InvalidRouteException $e) {
+            return null;
         }
     }
 
-    public function findCallbackRoute()
+    public function findCallbackRoute(): ?SymfonyRoute
     {
         $callbackRoutes = $this->getRouteCollection()->findByType(CallbackRoute::class);
 
         /** @var CallbackRoute $callbackRoute */
         foreach ($callbackRoutes->all() as $callbackRoute) {
-            if (
-                apply_filters('offbeatwp/route/match/wp', true, $callbackRoute) &&
-                $callbackRoute->doMatchCallback()
-            ) {
+            $filteredRoute = apply_filters('offbeatwp/route/match/wp', true, $callbackRoute);
+
+            if ($filteredRoute && $callbackRoute->doMatchCallback()) {
                 return $callbackRoute;
             }
         }
 
-        return false;
+        return null;
     }
 
-    public function removeRoute($route)
+    public function removeRoute(Route $route): void
     {
-        if ($route instanceof Route) {
-            $routeName = $route->getName();
-        }
-
-        if (
-            $route instanceof Route &&
-            $route->getOption('persistent') === true
-        ) {
+        if ($route->getOption('persistent') === true) {
             return;
         }
 
-        if (!empty($routeName)) {
-            $this->getRouteCollection()->remove($routeName);
-        }
+        $this->getRouteCollection()->remove($route->getName());
     }
 
-    public function getLastMatchRoute()
+    public function getLastMatchRoute(): ?SymfonyRoute
     {
         return $this->lastMatchRoute;
     }
