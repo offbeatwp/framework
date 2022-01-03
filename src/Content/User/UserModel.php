6c722dc2c21316334d3f765921e8412b92cbdab8
Maikel Haarmans <maikel@raow.nl>
2022-01-03 14:13:36 +0100

    Allow initialising empty user and add some helper methods for the current user

--- src/Content/User/UserModel.php
+++ src/Content/User/UserModel.php
@@ -9,15 +9,19 @@ class UserModel
 {
     protected $wpUser;
 
-    /** @var WP_User|int */
-    public function __construct($user)
+    /** @var WP_User|int|null */
+    public function __construct($user = null)
     {
+        if ($user === null) {
+            $user = new WP_User();
+        }
+
         if (is_int($user)) {
             $user = get_userdata($user);
         }
 
         if (!$user instanceof WP_User) {
-            throw new UserModelException('UserModel constructor requires a WP_User as parameter, or an integer represesting the ID of an existing user.');
+            throw new UserModelException('UserModel constructor requires a WP_User as parameter, or an integer representing the ID of an existing user.');
         }
 
         $this->wpUser = $user;
@@ -26,6 +30,30 @@ class UserModel
     public function getWpUser(): WP_User
     {
         return $this->wpUser;
+    }
+
+    /** Returns true if this user is the currently logged in user. */
+    public function isCurrentUser(): bool
+    {
+        return (get_current_user_id() && $this->wpUser->ID === get_current_user_id());
+    }
+
+    /**
+     * Returns the user that is currently logged in, or null if no user is logged in.
+     * @return static|null
+     */
+    public static function getCurrentUser(): ?UserModel
+    {
+        return self::query()->findById(get_current_user_id());
+    }
+
+    /**
+     * Returns the user that is currently logged in, or null if no user is logged in.
+     * @return static
+     */
+    public static function getCurrentUserOrFail(): UserModel
+    {
+        return self::query()->findByIdOrFail(get_current_user_id());
     }
 
     public static function query(): UserQueryBuilder
