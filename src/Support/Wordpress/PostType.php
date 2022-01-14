7a3acab872475328bfa6a938137f91aa571fbb2b
Maikel Haarmans <maikel@raow.nl>
2022-01-14 17:16:46 +0100

    This one seems a bit too unlikely to get used, so I'm removing it again

--- src/Support/Wordpress/PostType.php
+++ src/Support/Wordpress/PostType.php
@@ -36,10 +36,4 @@ class PostType
     {
         return array_search($modelClass, $this->postTypeModels, true);
     }
-
-    public static function createModelFromPostType(string $postType): PostModel
-    {
-        $modelClass = (new PostType())->getModelByPostType($postType);
-        return new $modelClass();
-    }
 }
