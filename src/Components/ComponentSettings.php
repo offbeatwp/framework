602cda4f4efb6ddbd6b72c811ed4e0e3c89d746e
Maikel Haarmans <maikel@raow.nl>
2022-03-16 14:54:58 +0100

    Add getUrlParam option

--- src/Components/ComponentSettings.php
+++ src/Components/ComponentSettings.php
@@ -18,19 +18,25 @@ final class ComponentSettings
     /**
      * Returns the value of the component setting or the default value of the setting if it does not exist.
      * @param non-empty-string $index The index of the value to retrieve.
-     * @param bool $getUrlParam When true, get the value from a URL param if it exists. Defaults to <i>false</i>.
      * @return mixed
      */
-    public function get(string $index, bool $getUrlParam = false)
+    public function get(string $index)
     {
-        if ($getUrlParam && isset($_GET[$index])) {
-            return $_GET[$index];
-        }
-
         if (property_exists($this, $index)) {
             return $this->$index;
         }
 
         return $this->defaultValues[$index] ?? null;
+    }
+
+    /**
+     * Return the value form a url param.
+     * If the url param does not exist, the component setting value is returned instead.
+     * @param non-empty-string $index
+     * @return mixed
+     */
+    public function getUrlParam(string $index)
+    {
+        return $_GET[$index] ?? $this->get($index);
     }
 }
