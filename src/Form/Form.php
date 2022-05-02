f0c46ab0e258607788dab08f885c058e51df4385
Maikel Haarmans <maikel@raow.nl>
2022-05-02 14:28:41 +0200

    The form class itself is no longer static

--- src/Form/Form.php
+++ src/Form/Form.php
@@ -11,7 +11,7 @@ use OffbeatWP\Form\FieldsContainers\Section;
 use OffbeatWP\Form\FieldsContainers\Tab;
 use OffbeatWP\Form\Fields\FieldInterface;
 
-final class Form extends Collection
+class Form extends Collection
 {
     /** @var Form|FieldsContainerInterface */
     private $activeItem;
