ecf194b7ddff2f19de26fb9e293de3d9f9a8c3ce
Maikel Haarmans <maikel@raow.nl>
2022-05-13 18:44:39 +0200

    Fix bug in TermQueryBuilder ids method

--- src/Content/Taxonomy/TermQueryBuilder.php
+++ src/Content/Taxonomy/TermQueryBuilder.php
@@ -103,7 +103,7 @@ class TermQueryBuilder
     /** @return int[] */
     public function ids(): array
     {
-        $this->queryVars['number'] = $this->queryVars['number'] ?? -1;
+        $this->queryVars['number'] = $this->queryVars['number'] ?? 0;
         $this->queryVars['fields'] = 'ids';
         $this->queryVars['no_found_rows'] = true;
 
