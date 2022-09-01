5444e83e8ca0bc479971f0a618477de01e33e123
Maikel Haarmans <maikel@raow.nl>
2022-09-01 10:00:00 +0200

    The OffbeatModelNotFoundException class will now use 404 as default code.

--- src/Exceptions/OffbeatModelNotFoundException.php
+++ src/Exceptions/OffbeatModelNotFoundException.php
@@ -2,7 +2,13 @@
 
 namespace OffbeatWP\Exceptions;
 
+use Throwable;
+
 class OffbeatModelNotFoundException extends OffbeatException
 {
-
+    /** @inheritDoc */
+    public function __construct($message = '', $code = 404, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
