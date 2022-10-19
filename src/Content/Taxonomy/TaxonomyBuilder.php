bf74b71cf006ddfd03c75af2cca9aaef751f1aae
Maikel Haarmans <maikel@raow.nl>
2022-10-19 12:38:51 +0200

    Add GraphQL support to taxonomies too

--- src/Content/Taxonomy/TaxonomyBuilder.php
+++ src/Content/Taxonomy/TaxonomyBuilder.php
@@ -6,13 +6,20 @@ class TaxonomyBuilder
 {
     /** @var string|null */
     private $taxonomy = null;
-    /** @var array|string|null */
+    /** @var string[]|null */
     private $postTypes = null;
-    /** @var array|null */
+    /** @var array */
     private $args = [];
     /** @var class-string|null */
     private $modelClass = null;
 
+    /**
+     * @param string $taxonomy
+     * @param string|string[] $postTypes
+     * @param string $pluralName
+     * @param string $singularLabel
+     * @return $this
+     */
     public function make($taxonomy, $postTypes, $pluralName, $singularLabel): TaxonomyBuilder
     {
         $this->taxonomy = $taxonomy;
@@ -239,6 +246,19 @@ class TaxonomyBuilder
         add_action('admin_footer-post-new.php', function () {
             $this->_hideTermAddWrap($this->taxonomy);
         });
+
+        return $this;
+    }
+
+    /**
+     * @param string $singleName Must be CamelCase.
+     * @param string $pluralName Must be CamelCase. Defaults to singlename if not omitted.
+     */
+    public function showInGraphQl(string $singleName, string $pluralName = ''): TaxonomyBuilder
+    {
+        $this->args['show_in_graphql'] = true;
+        $this->args['graphql_single_name'] = $singleName;
+        $this->args['graphql_plural_name'] = $pluralName ?: $singleName;
 
         return $this;
     }
