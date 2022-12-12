14e07fa821d992e8cac4bb3fa22fa4c8b0b32eb6
Maikel Haarmans <maikel@raow.nl>
2022-12-12 13:50:38 +0100

    Add some comments

--- src/Content/Post/WpPostsIterator.php
+++ src/Content/Post/WpPostsIterator.php
@@ -36,11 +36,17 @@ class WpPostsIterator implements Iterator
         next($this->items);
     }
 
+    /**
+     * Many WordPress methods rely on the global post object.<br>
+     * In the valid method we setup the global post object to ensure that these methods work properly during this iteration of the loop.
+     * @return bool
+     */
     public function valid(): bool
     {
         if (key($this->items) !== null) {
             $item = current($this->items)->wpPost;
 
+            // Remember the old value of the post global so that we can put it back after the loop is finished.
             if (!$this->globalPostWasChanged) {
                 $this->globalPostWasChanged = true;
                 $this->originalPost = $GLOBALS['post'] ?? null;
@@ -52,6 +58,7 @@ class WpPostsIterator implements Iterator
             return true;
         }
 
+        // The loop is finished; put back the global post object to the value it had before the loop was started.
         if ($this->globalPostWasChanged) {
             $GLOBALS['post'] = $this->originalPost;
             $this->globalPostWasChanged = false;
