06ee3bc2a2ef6bfdd173b7933a5e0100f34a2e6c
Maikel Haarmans <maikel@raow.nl>
2023-01-13 17:49:51 +0100

    Add a whole bunch of comments to Wordpress.php to document how certain methods work.

--- src/Views/Wordpress.php
+++ src/Views/Wordpress.php
@@ -28,58 +28,124 @@ class Wordpress
         return wp_title('&raquo;', false);
     }
 
+    /**
+     * Gets the language attributes for the 'html' tag.
+     * Builds up a set of HTML attributes containing the text direction and language information for the page.
+     * @return string
+     */
     public function languageAttributes()
     {
         return get_language_attributes();
     }
 
+    /**
+     * Retrieves the permalink for a post type archive.
+     * @param string|null $postType
+     * @return false|string
+     */
     public function archiveUrl(?string $postType = 'post')
     {
         return get_post_type_archive_link($postType);
     }
 
+    /**
+     * Returns a navigation menu.
+     * @param array|null $args Optional. Array of nav menu arguments.
+     * @return false|string|null
+     */
     public function navMenu(?array $args = [])
     {
         $args['echo'] = false;
-
         return wp_nav_menu($args);
     }
 
+    /**
+     * Retrieves the URL for the current site.<br>
+     * Returns the 'home' option with the appropriate protocol.<br>
+     * The protocol will be 'https' if is_ssl() evaluates to true; otherwise, it will be the same as the 'home' option.
+     */
     public function homeUrl(): string
     {
         return get_home_url();
     }
 
+    /**
+     * Retrieves the URL for a given site where WordPress application files (e.g. wp-blog-header.php or the wp-admin/ folder) are accessible.<br>
+     * Returns the 'site_url' option with the appropriate protocol, 'https' if is_ssl() and 'http' otherwise.
+     * @param int|null $id
+     * @return string
+     */
     public function blogUrl(?int $id = null): string
     {
         return get_site_url($id);
     }
 
+    /** Retrieves stylesheet directory URI for the active theme. */
     public function themeRootUrl(): string
     {
         return get_stylesheet_directory_uri();
     }
 
+    /**
+     * Retrieves the URL for the current site where WordPress application files (e.g. wp-blog-header.php or the wp-admin/ folder) are accessible.<br>
+     * Returns the 'site_url' option with the appropriate protocol, 'https' if is_ssl() and 'http' otherwise.
+     * @return string
+     */
     public function siteUrl(): string
     {
         return site_url();
     }
 
+    /** Retrieve the current site ID. */
     public function blogId(): int
     {
         return get_current_blog_id();
     }
 
-    public function bloginfo(?string $name): string
+    /**
+     * Retrieves information about the current site.<br>
+     * Possible values for <b>$show</b> include:<br>
+     * - 'name' - Site title (set in Settings > General)
+     * - 'description' - Site tagline (set in Settings > General)
+     * - 'wpurl' - The WordPress address (URL) (set in Settings > General)
+     * - 'url' - The Site address (URL) (set in Settings > General)
+     * - 'admin_email' - Admin email (set in Settings > General)
+     * - 'charset' - The "Encoding for pages and feeds"  (set in Settings > Reading)
+     * - 'version' - The current WordPress version
+     * - 'html_type' - The content-type (default: "text/html"). Themes and plugins
+     *   can override the default value using the {@see 'pre_option_html_type'} filter
+     * - 'text_direction' - The text direction determined by the site's language. is_rtl()
+     *   should be used instead
+     * - 'language' - Language code for the current site
+     * - 'stylesheet_url' - URL to the stylesheet for the active theme. An active child theme
+     *   will take precedence over this value
+     * - 'stylesheet_directory' - Directory path for the active theme.  An active child theme
+     *   will take precedence over this value
+     * - 'template_url' / 'template_directory' - URL of the active theme's directory. An active
+     *   child theme will NOT take precedence over this value
+     * - 'pingback_url' - The pingback XML-RPC file URL (xmlrpc.php)
+     * - 'atom_url' - The Atom feed URL (/feed/atom)
+     * - 'rdf_url' - The RDF/RSS 1.0 feed URL (/feed/rdf)
+     * - 'rss_url' - The RSS 0.92 feed URL (/feed/rss)
+     * - 'rss2_url' - The RSS 2.0 feed URL (/feed)
+     * - 'comments_atom_url' - The comments Atom feed URL (/comments/feed)
+     * - 'comments_rss2_url' - The comments RSS 2.0 feed URL (/comments/feed)
+     *
+     * @param string|null $show   Optional. Site info to retrieve. Default empty (site name).
+     * @return string Mostly string values, might be empty.
+     */
+    public function bloginfo(?string $show): string
     {
-        return get_bloginfo($name, 'display');
+        return get_bloginfo($show ?? '', 'display');
     }
 
+    /** Retrieve the details for a blog from the blogs table and blog options. */
     public function blogDetails(int $id): ?WP_Site
     {
         return get_blog_details($id) ?: null;
     }
 
+    /** Echos the class names for the body element. */
     public function bodyClass($class = '')
     {
         body_class($class);
@@ -92,11 +158,22 @@ class Wordpress
         return ob_get_clean();
     }
 
+    /**
+     * Searches content for shortcodes and filter shortcodes through their hooks.<br>
+     * If there are no shortcode tags defined, then the content will be returned without any filtering.<br>
+     * This might cause issues when plugins are disabled but the shortcode will still show up in the post or content.
+     * @param string|null $code
+     * @return string
+     */
     public function shortcode(?string $code): string
     {
         return do_shortcode($code);
     }
 
+    /**
+     * @param int|string $name
+     * @return false|string
+     */
     public function sidebar($name)
     {
         ob_start();
@@ -104,6 +181,10 @@ class Wordpress
         return ob_get_clean();
     }
 
+    /**
+     * @param int|null $postId
+     * @return false|mixed
+     */
     public function getAllPostMeta(?int $postId = null)
     {
         if ($postId) {
@@ -132,6 +213,14 @@ class Wordpress
         return ($attachment) ? new OffbeatImageSrc($attachment) : null;
     }
 
+    /**
+     * Retrieves an attachment URL.
+     * While <b>$size</b> will accept an array, it is better to register a size with add_image_size() so that a cropped version is generated.<br>
+     * It's much more efficient than having to find the closest-sized image and then having the browser scale down the image.
+     * @param int|null $attachmentID
+     * @param string|int[] $size
+     * @return false|string
+     */
     public function attachmentUrl(?int $attachmentID, $size = 'full')
     {
         $attachment = wp_get_attachment_image_src($attachmentID, $size);
@@ -145,7 +234,7 @@ class Wordpress
 
     /**
      * Gets an HTML img element representing an image attachment.<br>
-     * While $size will accept an array, it is better to register a size with add_image_size() so that a cropped version is generated.<br>
+     * While <b>$size/b> will accept an array, it is better to register a size with add_image_size() so that a cropped version is generated.<br>
      * It's much more efficient than having to find the closest-sized image and then having the browser scale down the image.
      * @param int[]|string[]|string $attachmentID
      * @param array{0: int, 1: int}|string $size
@@ -176,6 +265,7 @@ class Wordpress
         return implode(', ', $srcSet);
     }
 
+    /** @deprecated */
     public function formatDate(?string $format, $date, bool $strtotime = false): string
     {
         if ($strtotime) {
@@ -185,11 +275,20 @@ class Wordpress
         return date_i18n($format, $date);
     }
 
+    /** After looping through a separate query, this function restores the $post global to the current post in the main query. */
     public function resetPostdata(): void
     {
         wp_reset_postdata();
     }
 
+    /**
+     * Determines whether the query is for the front page of the site.<br>
+     * This is for what is displayed at your site's main URL.<br>
+     * Depends on the site's “Front page displays” Reading Settings 'show_on_front' and 'page_on_front'.<br>
+     * If you set a static page for the front page of your site, this function will return true when viewing that page.<br>
+     * Otherwise the same as is_home().
+     * @see is_home()
+     */
     public function isFrontPage(): bool
     {
         return is_front_page();
@@ -206,16 +305,28 @@ class Wordpress
         return $url;
     }
 
+    /** Determines whether the query is for a search. */
     public function isSearchPage(): bool
     {
         return is_search();
     }
 
+    /**
+     * Retrieves the contents of the search WordPress query variable.<br>
+     * The search query string is passed through esc_attr() to ensure that it is safe for placing in an HTML attribute.
+     * @see esc_attr()
+     */
     public function getSearchQuery(): ?string
     {
         return ($this->isSearchPage()) ? get_search_query() : null;
     }
 
+    /**
+     * Retrieves the post title.<br>
+     * If the post is protected and the visitor is not an admin, then “Protected” will be inserted before the post title.<br>
+     * If the post is private, then “Private” will be inserted before the post title.
+     * @return string
+     */
     public function getPageTitle(): string
     {
         return get_the_title();
@@ -237,12 +348,13 @@ class Wordpress
         return get_queried_object_id();
     }
 
-    /** @return string|null Retrieves the customisable header image url. */
+    /** Retrieves the customisable header image url. */
     public function getHeaderImageUrl(): ?string
     {
         return get_header_image() ?: null;
     }
 
+    /** Test if the current browser runs on a mobile device (smart phone, tablet, etc.) */
     public function isMobile(): bool
     {
         return wp_is_mobile();
