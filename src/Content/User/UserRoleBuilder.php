3538ac3a3d1c635ed2a1c687af743e30cc91fd66
Maikel Haarmans <maikel@raow.nl>
2023-01-16 10:10:57 +0100

    UserRoleBuilder can now be provided with capabilities.

--- src/Content/User/UserRoleBuilder.php
+++ src/Content/User/UserRoleBuilder.php
@@ -4,12 +4,13 @@ namespace OffbeatWP\Content\User;
 
 use OffbeatWP\Support\Wordpress\UserRole;
 
-class UserRoleBuilder
+final class UserRoleBuilder
 {
-    private $roleName;
-    private $roleDisplayName;
-    /** @var class-string<UserModel> */
-    private $modelClass;
+    /** @var class-string<UserModel>|null */
+    private ?string $modelClass = null;
+    private string $roleName;
+    private string $roleDisplayName;
+    private array $capabilities = [];
 
     private function __construct(string $roleName, string $roleDisplayName)
     {
@@ -27,6 +28,54 @@ class UserRoleBuilder
         return $this;
     }
 
+    /**
+     * Add a capability to a role. The default WordPress capabilities include:<br>
+     * activate_plugins
+     * delete_others_pages
+     * delete_others_posts
+     * delete_pages
+     * delete_posts
+     * delete_private_pages
+     * delete_private_posts
+     * delete_published_pages
+     * delete_published_posts
+     * edit_dashboard
+     * edit_others_pages
+     * edit_others_posts
+     * edit_pages
+     * edit_posts
+     * edit_private_pages
+     * edit_private_posts
+     * edit_published_pages
+     * edit_published_posts
+     * edit_theme_options
+     * export
+     * import
+     * list_users
+     * manage_categories
+     * manage_links
+     * manage_options
+     * moderate_comments
+     * promote_users
+     * publish_pages
+     * publish_posts
+     * read_private_pages
+     * read_private_posts
+     * remove_users
+     * switch_themes
+     * upload_files
+     * customize
+     * delete_site
+     * @param string $capabilityName
+     * @param bool $hasCapability
+     * @return $this
+     */
+    public function setCapability(string $capabilityName, bool $hasCapability): UserRoleBuilder
+    {
+        $this->capabilities[$capabilityName] = $hasCapability;
+        return $this;
+    }
+
     public static function make(string $roleName, string $roleDisplayName): UserRoleBuilder
     {
         return new static($roleName, $roleDisplayName);
@@ -34,7 +83,7 @@ class UserRoleBuilder
 
     public function set(): void
     {
-        add_role($this->roleName, $this->roleDisplayName);
+        add_role($this->roleName, $this->roleDisplayName, $this->capabilities);
 
         if ($this->modelClass !== null) {
             UserRole::registerUserRole($this->roleName, $this->modelClass);
