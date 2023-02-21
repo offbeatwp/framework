3808ed6a182ea56966e3451c8f9c3670984ce6fd
Maikel Haarmans <maikel@raow.nl>
2023-02-21 14:36:36 +0100

    Write docs for AbstractCommand.php

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -1,20 +1,43 @@
 <?php
 namespace OffbeatWP\Console;
 
+use Exception;
+use Throwable;
 use WP_CLI;
+use WP_Error;
 
 abstract class AbstractCommand {
+    /**
+     * @param string[] $args
+     * @param string[] $argsNamed
+     * @return mixed
+     */
     abstract public function execute($args, $argsNamed);
 
-    public function error($message) {
+    /**
+     * @param Exception|string|Throwable|WP_Error $message
+     * @return void
+     */
+    public function error($message)
+    {
         WP_CLI::error($message);
     }
 
-    public function log($message) {
+    /**
+     * @param string $message
+     * @return void
+     */
+    public function log($message)
+    {
         WP_CLI::log($message);
     }
 
-    public function success($message) {
+    /**
+     * @param string $message
+     * @return void
+     */
+    public function success($message)
+    {
         WP_CLI::success($message);
     }
 }
