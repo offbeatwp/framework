ea0cc3c393b2308ce9dbe9267131b6e326ed0ebc
Maikel Haarmans <maikel@raow.nl>
2023-04-06 17:31:58 +0200

    Typehint SiteSettings

--- src/Contracts/SiteSettings.php
+++ src/Contracts/SiteSettings.php
@@ -3,9 +3,17 @@
 namespace OffbeatWP\Contracts;
 
 interface SiteSettings {
+    /** @param class-string $class */
     public function addPage($class);
 
+    /** @param string $key */
     public function get($key);
 
+    /**
+     * Should return <b>true</b> on successful update, <b>false</b> on failure
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
     public function update($key, $value);
 }
