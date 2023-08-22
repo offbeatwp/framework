308dbb7b67cc8e2468def26a4655e95763a2b700
Maikel Haarmans <maikel@raow.nl>
2023-08-22 10:28:46 +0200

    Add a couple of docs

--- src/Services/AbstractService.php
+++ src/Services/AbstractService.php
@@ -1,9 +1,13 @@
 <?php
 namespace OffbeatWP\Services;
 
+use OffbeatWP\Foundation\App;
+
 abstract class AbstractService {
+    /** @var App */
     protected $app;
 
+    /** @param App $app */
     public function __construct($app)
     {
         $this->app = $app;
