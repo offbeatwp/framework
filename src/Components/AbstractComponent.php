0ee758427e8e940664f61b324b0aaf6517d3b6ae
Maikel Haarmans <maikel@raow.nl>
2023-09-12 10:08:42 +0200

    Fix doc

--- src/Components/AbstractComponent.php
+++ src/Components/AbstractComponent.php
@@ -53,7 +53,7 @@ abstract class AbstractComponent
      * *string* **icon** - The name of the dash-icon that this setting will use in the editor
      *
      * *string[]* **supports** - Supported functionality of this component. Valid options include 'pagebuilder', 'editor', 'shortcode' and 'widget'.
-     * @return array{name: string, description: string, slug: string, category: string, icon: string, supports: string[], block: mixed[]}
+     * @return array{name?: string, description?: string, slug: string, category?: string, icon?: string, supports?: string[], block?: mixed[]}
      */
     abstract static function settings();
 
