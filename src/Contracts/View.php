faf546895d24a647da8149931c294da8a2236576
Maikel Haarmans <maikel@raow.nl>
2024-04-18 16:09:38 +0200

    Temporarily revert Contract changes

--- src/Contracts/View.php
+++ src/Contracts/View.php
@@ -8,18 +8,18 @@ interface View
      * @param mixed[]|object $data
      * @return string|null
      */
-    public function render($template, $data = []): ?string;
+    public function render($template, $data = []);
 
     /**
      * @param string $namespace
      * @param mixed $value
      * @return void
      */
-    public function registerGlobal($namespace, $value): void;
+    public function registerGlobal($namespace, $value);
 
     /**
      * @param string $path
      * @return void
      */
-    public function addTemplatePath($path): void;
+    public function addTemplatePath($path);
 }
