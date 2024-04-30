670734effa801f941e043bae87aa1575cac48434
Maikel Haarmans <maikel@raow.nl>
2024-04-30 10:40:26 +0200

    Better deprecation message when calling QueryBuilders.

--- src/Content/Post/PostModel.php
+++ src/Content/Post/PostModel.php
@@ -122,7 +122,7 @@ class PostModel implements PostModelInterface
         }
 
         if (method_exists(WpQueryBuilderModel::class, $method)) {
-            trigger_error('Called a QueryBuilder method on a model instance through magic method.', E_USER_DEPRECATED);
+            trigger_error('Called WpQueryBuilder method on a model instance through magic method. Please use the static PostModel::query method instead.', E_USER_DEPRECATED);
             return static::query()->$method(...$parameters);
         }
 
