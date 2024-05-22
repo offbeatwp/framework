441295e18694b83257f41c3b2e037db7b3abeb62
Maikel Haarmans <maikel@raow.nl>
2024-05-22 10:22:56 +0200

    PathRoute is now an alias for Route

--- src/Routes/Routes/PathRoute.php
+++ src/Routes/Routes/PathRoute.php
@@ -3,17 +3,5 @@ namespace OffbeatWP\Routes\Routes;
 
 final class PathRoute extends Route
 {
-    public function __construct(string $name, string $path, $actionCallback, array $defaults = [], array $requirements = [], array $options = [], ?string $host = '', string|array $schemes = [], string|array $methods = [], ?string $condition = '') {
-        $this->setName($name);
-        $this->setActionCallback($actionCallback);
 
-        $this->setPath($path);
-        $this->addDefaults($defaults);
-        $this->addRequirements($requirements);
-        $this->setOptions($options);
-        $this->setHost($host);
-        $this->setSchemes($schemes);
-        $this->setMethods($methods);
-        $this->setCondition($condition);
-    }
 }
