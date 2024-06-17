e5fec86a80a0780a09d87fd3cd1ae2c3a88d53f2
Maikel Haarmans <maikel@raow.nl>
2024-06-17 09:13:17 +0200

    Type WP_REST_Request in RestEndpointBuilder.php

--- src/Api/RestEndpointBuilder.php
+++ src/Api/RestEndpointBuilder.php
@@ -8,15 +8,15 @@ final class RestEndpointBuilder
 {
     public string $namespace;
     public string $route;
-    /** @var callable(\WP_REST_Request): (\WP_REST_Response|\WP_Error) */
+    /** @var callable(\WP_REST_Request<mixed[]>): (\WP_REST_Response|\WP_Error) */
     public $callback;
     public string $method = WP_REST_Server::READABLE;
     /** @var \Closure[] */
     public array $args = [];
-    /** @var callable(\WP_REST_Request): (bool|\WP_Error) */
+    /** @var callable(\WP_REST_Request<mixed[]>): (bool|\WP_Error) */
     public $permissionCallback = '__return_true';
 
-    /** @param callable(\WP_REST_Request): mixed $callback */
+    /** @param callable(\WP_REST_Request<mixed[]>): mixed $callback */
     public function __construct(string $namespace, string $route, callable $callback)
     {
         $this->namespace = $namespace;
@@ -33,7 +33,7 @@ final class RestEndpointBuilder
 
     /**
      * Validate a particular endpoint argument against a callback.
-     * @param callable(string, \WP_REST_Request, string): bool $callback
+     * @param callable(string, \WP_REST_Request<mixed[]>, string): bool $callback
      * @return $this
      */
     public function validate(string $key, callable $callback)
@@ -61,7 +61,7 @@ final class RestEndpointBuilder
      * Adds a callback that checks if the user can perform the action (reading, updating, etc) before the real callback is called.<br>
      * This allows the API to tell the client what actions they can perform on a given URL without needing to attempt the request first.<br>
      * The permissions callback is run after remote authentication, which sets the current user.
-     * @param callable(\WP_REST_Request): (bool|\WP_Error) $callback
+     * @param callable(\WP_REST_Request<mixed[]>): (bool|\WP_Error) $callback
      * @return $this
      */
     public function permission(callable $callback)
@@ -84,19 +84,19 @@ final class RestEndpointBuilder
         });
     }
 
-    /** @param callable(\WP_REST_Request): (\WP_REST_Response|\WP_Error) $callback */
+    /** @param callable(\WP_REST_Request<mixed[]>): (\WP_REST_Response|\WP_Error) $callback */
     public static function get(string $namespace, string $route, callable $callback): static
     {
         return new static($namespace, $route, $callback);
     }
 
-    /** @param callable(\WP_REST_Request): (\WP_REST_Response|\WP_Error) $callback */
+    /** @param callable(\WP_REST_Request<mixed[]>): (\WP_REST_Response|\WP_Error) $callback */
     public static function post(string $namespace, string $route, callable $callback): static
     {
         return (new static($namespace, $route, $callback))->method(WP_REST_Server::CREATABLE);
     }
 
-    /** @param callable(\WP_REST_Request): (\WP_REST_Response|\WP_Error) $callback */
+    /** @param callable(\WP_REST_Request<mixed[]>): (\WP_REST_Response|\WP_Error) $callback */
     public static function delete(string $namespace, string $route, callable $callback): static
     {
         return (new static($namespace, $route, $callback))->method(WP_REST_Server::DELETABLE);
