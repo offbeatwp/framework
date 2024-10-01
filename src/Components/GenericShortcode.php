d1ee20f94c36103bf52d81b5652305cfafe08479
Maikel Haarmans <maikel@raow.nl>
2024-10-01 16:14:47 +0200

    Add missing docs

--- src/Components/GenericShortcode.php
+++ src/Components/GenericShortcode.php
@@ -6,13 +6,20 @@ class GenericShortcode
 {
     use ComponentInterfaceTrait;
 
+    /** @var class-string|null */
     public $componentClass = null;
 
+    /** @param class-string $componentClass */
     public function __construct($componentClass)
     {
         $this->componentClass = $componentClass;
     }
 
+    /**
+     * @param mixed[]|null $atts
+     * @param string $content
+     * @return string|null
+     */
     public function renderShortcode($atts, $content = '')
     {
         if (!is_array($atts)) {
