138953530dc01a08ea2858291f0aeef255bb951f
Maikel Haarmans <maikel@raow.nl>
2025-06-19 09:10:04 +0200

    owp-options.php

--- src/Foundation/owp-options.php
+++ src/Foundation/owp-options.php
@@ -1,30 +1,21 @@
 <?php
 
-if (!function_exists('owp_get_option_string')) {
-    function owp_get_option_string(string $option): ?string
-    {
-        return filter_var(get_option($option, null), FILTER_DEFAULT,  FILTER_NULL_ON_FAILURE);
-    }
+function owp_get_option_string(string $option): ?string
+{
+    return filter_var(get_option($option, null), FILTER_DEFAULT,  FILTER_NULL_ON_FAILURE);
 }
 
-if (!function_exists('owp_get_option_int')) {
-    function owp_get_option_int(string $option): ?int
-    {
-        return filter_var(get_option($option, null), FILTER_VALIDATE_INT,  FILTER_NULL_ON_FAILURE);
-    }
+function owp_get_option_int(string $option): ?int
+{
+    return filter_var(get_option($option, null), FILTER_VALIDATE_INT,  FILTER_NULL_ON_FAILURE);
 }
 
-if (!function_exists('owp_get_option_float')) {
-    function owp_get_option_float(string $option): ?float
-    {
-        return filter_var(get_option($option, null), FILTER_VALIDATE_FLOAT,  FILTER_NULL_ON_FAILURE);
-    }
+function owp_get_option_float(string $option): ?float
+{
+    return filter_var(get_option($option, null), FILTER_VALIDATE_FLOAT,  FILTER_NULL_ON_FAILURE);
 }
 
-
-if (!function_exists('owp_get_option_bool')) {
-    function owp_get_option_bool(string $option): ?bool
-    {
-        return filter_var(get_option($option, null), FILTER_VALIDATE_BOOL,  FILTER_NULL_ON_FAILURE);
-    }
+function owp_get_option_bool(string $option): ?bool
+{
+    return filter_var(get_option($option, null), FILTER_VALIDATE_BOOL,  FILTER_NULL_ON_FAILURE);
 }
