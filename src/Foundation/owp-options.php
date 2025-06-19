bf838ec83359b67a01ec2580f2ff23c189b10b1f
Maikel Haarmans <maikel@raow.nl>
2025-06-19 12:07:36 +0200

    Add owp_get_option_id

--- src/Foundation/owp-options.php
+++ src/Foundation/owp-options.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Retrieves an option string value based on an option name.
+ * Retrieves a string value based on an option name.
  *
- * If the option does not exist or is not a string, <i>NULL</i> is returned.
+ * If the option does not exist or is not a string then <i>NULL</i> is returned.
  * @param non-falsy-string $option
  */
 function owp_get_option_string(string $option): ?string
@@ -12,9 +12,9 @@ function owp_get_option_string(string $option): ?string
 }
 
 /**
- * Retrieves an option integer value based on an option name.
+ * Retrieves an integer value based on an option name.
  *
- * If the option does not exist or is not an integer, <i>NULL</i> is returned.
+ * If the option does not exist or is not an integer then <i>NULL</i> is returned.
  * @param non-falsy-string $option
  */
 function owp_get_option_int(string $option): ?int
@@ -23,9 +23,9 @@ function owp_get_option_int(string $option): ?int
 }
 
 /**
- * Retrieves an option float value based on an option name.
+ * Retrieves a float value based on an option name.
  *
- * If the option does not exist or is not a float, <i>NULL</i> is returned.
+ * If the option does not exist or is not a float then <i>NULL</i> is returned.
  * @param non-falsy-string $option
  */
 function owp_get_option_float(string $option): ?float
@@ -34,12 +34,25 @@ function owp_get_option_float(string $option): ?float
 }
 
 /**
- * Retrieves an option boolean value based on an option name.
+ * Retrieves a boolean value based on an option name.
  *
- * If the option does not exist or is not a boolean, <i>NULL</i> is returned.
+ * If the option does not exist or is not a boolean then <i>NULL</i> is returned.
  * @param non-falsy-string $option
  */
 function owp_get_option_bool(string $option): ?bool
 {
     return filter_var(get_option($option, null), FILTER_VALIDATE_BOOL,  FILTER_NULL_ON_FAILURE);
+}
+
+/**
+ * Retrieves a positive integer value based on an option name.
+ *
+ * If the option does not exist, is not an integer or is <= 0 then <i>NULL</i> is returned.
+ * @param non-falsy-string $option
+ * @return positive-int|null
+ */
+function owp_get_option_id(string $option): ?int
+{
+    $v = owp_get_option_int($option);
+    return $v && $v > 0 ? $v : null;
 }
