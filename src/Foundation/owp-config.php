f6bf47b5510bc34aa2919124578dc16ecaabd46d
Maikel Haarmans <maikel@raow.nl>
2025-09-26 13:50:21 +0200

    Push owp config

---
+++ src/Foundation/owp-config.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Retrieves a string value based on an option name.
+ *
+ * If the option does not exist or is not a string then <i>NULL</i> is returned.
+ * @param non-falsy-string $config
+ */
+function owp_get_config_string(string $config): ?string
+{
+    return filter_var(config($config), FILTER_DEFAULT, FILTER_NULL_ON_FAILURE);
+}
+
+/**
+ * Retrieves an integer value based on an option name.
+ *
+ * If the option does not exist or is not an integer then <i>NULL</i> is returned.
+ * @param non-falsy-string $config
+ */
+function owp_get_config_int(string $config): ?int
+{
+    return filter_var(config($config), FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
+}
+
+/**
+ * Retrieves a float value based on an option name.
+ *
+ * If the option does not exist or is not a float then <i>NULL</i> is returned.
+ * @param non-falsy-string $config
+ */
+function owp_get_config_float(string $config): ?float
+{
+    return filter_var(config($config), FILTER_VALIDATE_FLOAT, FILTER_NULL_ON_FAILURE);
+}
+
+/**
+ * Retrieves a boolean value based on an option name.
+ *
+ * If the option does not exist or is not a boolean then <i>NULL</i> is returned.
+ * @param non-falsy-string $config
+ */
+function owp_get_config_bool(string $config): ?bool
+{
+    return filter_var(config($config), FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
+}
+
+/**
+ * Retrieves a positive integer value based on an option name.
+ *
+ * If the option does not exist, is not an integer or is <= 0 then <i>NULL</i> is returned.
+ * @param non-falsy-string $config
+ * @return positive-int|null
+ */
+function owp_get_config_positive_int(string $config): ?int
+{
+    $v = owp_get_config_int($config);
+    return $v && $v > 0 ? $v : null;
+}
